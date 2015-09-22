ee71c14212bd3ff20c5b61e471e095c3f06cfc6c
Oleksandr Ievstratiev <oievstratiev@productengine.com>
2015-09-22 17:06:36 +0300

    Added servlet support

--- src/main/java/net/odtel/knowledgy/config/ServletContextConfig.java
+++ src/main/java/net/odtel/knowledgy/config/ServletContextConfig.java
@@ -1,10 +1,14 @@
 package net.odtel.knowledgy.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @Configuration
 @EnableWebMvc
@@ -12,8 +16,29 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 public class ServletContextConfig extends WebMvcConfigurerAdapter {
 
     @Override
+    public void addViewControllers (ViewControllerRegistry registry) {
+        registry.addViewController("/").setViewName("/main");
+        registry.addViewController("/uncaughtException");
+    }
+
+    @Bean
+    public InternalResourceViewResolver internalResourceViewResolver () {
+        InternalResourceViewResolver internalResourceViewResolver = new InternalResourceViewResolver();
+        internalResourceViewResolver.setPrefix("/resources/");
+        internalResourceViewResolver.setSuffix(".html");
+        return internalResourceViewResolver;
+    }
+
+    @Override
     public void addResourceHandlers (ResourceHandlerRegistry registry) {
         super.addResourceHandlers(registry);
-        registry.addResourceHandler("/resources/**").addResourceLocations("/resources");
+        registry.addResourceHandler("/resources").addResourceLocations("**");
     }
+
+    @Override
+    public void configureDefaultServletHandling (DefaultServletHandlerConfigurer configurer) {
+        configurer.enable();
+    }
+
+
 }
